357c83bebb8570be0078de8738e4293af4b26013
salvador <salva@anvimur.com>
2021-05-02 12:34:43 +0200

    V2

---
+++ laravel-vue/resources/views/form.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Formulario Empresa</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        
+    </head>
+    <body>
+    <div class="row">
+        <div class="col-md-8 order-md-1">
+            <div class="col-md-7 mb-3">
+                <h1>Deja tus datos y nos pondremos en contacto</h1>
+            </div>
+            <form class="needs-validation" novalidate="">
+                <!--<div class="row">-->
+                    <div class="col-md-6 mb-3">
+                        <label for="nombre">¿Como se llama tu empresa?</label>
+                        <input class="form-control" id="nombre" placeholder="Empresa 5" type="text" name="nombre"/>
+                    </div>
+                <!--</div>-->
+                <div class="col-md-6 mb-3">
+                    <label for="email">¿Cuales son tus ingresos anuales?</label>
+                    <input class="form-control"  id="email" type="text" placeholder="15.000" name="email"/>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="email">¿Como te llamas tú?</label>
+                    <input class="form-control"  id="email" type="text" placeholder="Pedro" name="email"/>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="email">Teléfono de contacto</label>
+                    <input class="form-control"  id="email" type="text" placeholder="666565" name="email"/>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="email">¿Qué te interesa de nosotros?</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Me interesan vuestros productos" rows="3"></textarea>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label>
+                        <input type="checkbox"> Acepto los terminos legales
+                    </label>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!--<script src="{{ asset('js/app.js') }}"></script>-->
+    </body>
+</html>
---
+++ laravel-vue/resources/views/gestor.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Gestor</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <link rel="stylesheet" href="../sass/gestor.css">
+
+    </head>
+    <body>
+        <div class="col-md-6 mb-3">
+            <div class="input-group">
+                <div class="form-outline">
+                    <input type="search" class="rounded" id="form1" placeholder="Filtrar por nomnre de la empresa" class="form-control" />
+                </div>
+            </div>
+        </div>
+   
+        <div class="col-md-12 mb-3"> 
+            <table class="table table-bordered">
+                <thead>
+                    <tr class="table-active">
+                        <th scope="col">id</th>
+                        <th scope="col">Empresa</th>
+                        <th scope="col" data-sortable="true" >Ingresos anuales</th>
+                        <th scope="col">Contacto</th>
+                        <th scope="col">Fecha de consulta</th>
+                        <th scope="col">Gestionado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Empresa 1</td>
+                        <td>1000€</td>
+                        <td>Pedro (65547879)</td>
+                        <td>6 de marzo a las 11:30</td>
+                        <td>SI</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Empresa 2</td>
+                        <td>1000€</td>
+                        <td>Pedro (65547879)</td>
+                        <td>6 de marzo a las 10:30</td>
+                        <td>NO</td>
+                    </tr>
+                
+            </tbody>
+        </table>
+      </div>
+
+    <!--<script src="{{ asset('js/app.js') }}"></script>-->
+    </body>
+</html>
